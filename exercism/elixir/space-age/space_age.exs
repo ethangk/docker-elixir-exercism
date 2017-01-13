f8110cee566d08fa8b9b62660994cc8307388e21
Ethan Katzenberg <ethangk@gmail.com>
2017-01-13 08:14:33 -0800

    Had a working solution with a big case statement, trying to clean it up

--- exercism/elixir/space-age/space_age.exs
+++ exercism/elixir/space-age/space_age.exs
@@ -2,12 +2,21 @@ defmodule SpaceAge do
   @type planet :: :mercury | :venus | :earth | :mars | :jupiter
                 | :saturn | :uranus | :neptune
 
+  @earth_year 31557600
+  @offsets %{
+    earth: 1, mercury: 0.2408467, venus: 0.61519726, mars: 1.8808158,
+    jupiter: 11.862615, saturn: 29.447498, uranus: 84.016846, neptune: 164.79132
+  }
+
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-
+    case Map.get(@offsets, planet) do
+      nil -> Raise "Something"
+      _ -> seconds / (@earth_year * @offsets[planet])
+    end
   end
 end
