cc136bc6c5bf9cc33d94be7cf0d818dad0cf796c
Ethan Katzenberg <Ethan.Katzenberg@loopup.com>
2017-01-13 18:43:15 -0800

    Added conditions for the hey function

--- exercism/elixir/bob/bob.exs
+++ exercism/elixir/bob/bob.exs
@@ -1,8 +1,26 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+        is_empty?(input) -> "Fine. Be that way!"
+        is_question?(input) -> "Sure."
+        is_shouting?(input) -> "Whoa, chill out!"
+        true -> "Whatever."
     end
+  end
+
+  def is_shouting?(input) do
+    filteredInputLength = input
+                            |> to_charlist
+                            |> Enum.filter(&(&1 > 122 || &1 < 97))
+                            |> length
+    String.length(input) == filteredInputLength
+  end
+
+  def is_empty?(input) do
+    input |> String.strip |> String.length == 0
+  end
+
+  def is_question?(input) do
+    input |> String.ends_with?("?")
   end
 end
