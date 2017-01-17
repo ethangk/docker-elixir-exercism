961fe09db369f19f0a7f3107ec64c685acace8f4
Ethan Katzenberg <ethangk@gmail.com>
2017-01-17 08:05:09 -0800

    Partially working with a reducer

--- exercism/elixir/word-count/word_count.exs
+++ exercism/elixir/word-count/word_count.exs
@@ -6,6 +6,7 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
-
+    words = sentence |> String.downcase |> String.split
+    words |> Enum.reduce(%{}, fn word, acc -> Map.update(acc, word, 1, &(&1 + 1)) end)
   end
 end
